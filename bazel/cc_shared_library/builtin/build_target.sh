26b340fead19317b28e5fb3d29442c5a7b4171b3
Rostam Dinyari <rostam@google.com>
2022-06-23 22:37:05 +0000

    Cleans comments in Shell script.

--- bazel/cc_shared_library/builtin/build_target.sh
+++ bazel/cc_shared_library/builtin/build_target.sh
@@ -1,11 +1,6 @@
 # Examples:
 # source build_target.sh hello_world:hello-world-shared
 
-# To create a debug file add: --experimental_cc_shared_library_debug \
-# Alternative way(s) to run Bazel:
-#   USE_BAZEL_VERSION=rolling bazelisk build \
-#   USE_BAZEL_VERSION=last_green bazelisk build \
-#   ~/Workspace/bazel/bazel/bazel-bin/src/bazel build \
 USE_BAZEL_VERSION=last_green bazelisk build \
   -c dbg \
   --experimental_cc_shared_library \
