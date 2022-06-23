99ca86aa348b51a56f31dd5d830e784e08c7974d
Rostam Dinyari <rostam@google.com>
2022-06-23 02:57:20 +0000

    Adds flag to generate debug files for cc_shared_library

--- bazel/cc_shared_library/builtin/build_target.sh
+++ bazel/cc_shared_library/builtin/build_target.sh
@@ -11,6 +11,7 @@ USE_BAZEL_VERSION=last_green bazelisk build \
   --experimental_cc_shared_library \
   --experimental_link_static_libraries_once=true \
   --experimental_cc_shared_library_debug \
+  --output_groups=rule_impl_debug_files \
   --experimental_debug_spawn_scheduler \
   --subcommands=pretty_print \
   --verbose_failures \
