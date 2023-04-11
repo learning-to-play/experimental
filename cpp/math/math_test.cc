14fd7124071ea801b3d981cb438ec4bca3713f01
Rostam Dinyari <rostam@google.com>
2023-04-11 00:48:49 +0000

    Adds an error message to test

--- cpp/math/math_test.cc
+++ cpp/math/math_test.cc
@@ -3,6 +3,6 @@
 
 TEST(Add, CorrectResult) {
     ASSERT_EQ(Add(0, 0), 0);
-    ASSERT_EQ(Add(0, 1), 1);
     ASSERT_EQ(Add(1, 2), 3);
+    ASSERT_EQ(Add(-1, -2), -3) << "Incorrect result for adding negative integers";
 }
