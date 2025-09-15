2ea55121579a04f93eccb7403a046c2b46923da9
Rostam Dinyari <rostam@google.com>
2025-09-15 00:34:50 +0000

    Copying through assignment in C++.

--- cpp/ptr/raw_ptr/main.cc
+++ cpp/ptr/raw_ptr/main.cc
@@ -1,15 +1,21 @@
+#include <cstdint>
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
-    int value = 42;
-    int* raw_ptr0 = &value;
+    int16_t value = 42;
+    int16_t* raw_ptr0 = &value;
+    cout << "&value:" << &value << endl;
     cout << "raw_ptr0:" << raw_ptr0 << endl;
     cout << "*raw_ptr0:" << *raw_ptr0 << endl;
- 
-    int* raw_ptr1 = new int[2];
+
+    // = operator copies value
+    int16_t value_copy = value;
+    cout << "&value_copy:" << &value_copy << endl;
+
+    int16_t* raw_ptr1 = new int16_t[2];
     raw_ptr1[0] = 0;
     raw_ptr1[1] = 1;
     cout << "raw_ptr1:" << raw_ptr1 << endl;
